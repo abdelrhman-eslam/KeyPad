8ebe2fd08576375278b9499c40f271101ee27207
Abdelrhman.Eslam <abdelrahmaneslam33@gmail.com>
2023-08-18 03:22:28 +0300

    v.2 -> includ 4x4 array

--- HAL/KeyPad/KPD_int.h
+++ HAL/KeyPad/KPD_int.h
@@ -8,7 +8,18 @@
 #ifndef HAL_KEYPAD_KPD_INT_H_
 #define HAL_KEYPAD_KPD_INT_H_
 #include "../../LIB/STD_TYPES.h"
+#include "../../MCAL/DIO/DIO.h"
+#include "util/delay.h"
+
+
+
 #define KPD_PORT	PORTA	//config.h
+
+#define RowNumber 4
+#define ColNumber 4
+
+u8 G_u8PortNum;
+
 
 void HKPD_voidInit();
 u8 HKPD_u8GetPressedKey();
--- HAL/KeyPad/KPD_prog.c
+++ HAL/KeyPad/KPD_prog.c
@@ -5,14 +5,16 @@
  *      Author: ENG.AbdelrhmanEslam
  */
 
-#include "../../LIB/STD_TYPES.h"
-#include "../../MCAL/DIO/DIO.h"
-#include "util/delay.h"
+
 #include "KPD_int.h"
 
 
-static const u8 GKPD_KEYS[16]={'1','4','7','c','2','5','8','0','3','6','9','=','/','*','-','+'};
-
+u8 G_u8Grid[4][4]={
+		{'7','8','9','/'},
+		{'4','5','6','*'},
+		{'1','2','3','-'},
+		{'c','0','=','+'}
+};
 void HKPD_voidInit(){
 
 	MDIO_voidSetPortDirection(KPD_PORT, 0x0f);
@@ -22,22 +24,23 @@ void HKPD_voidInit(){
 
 u8 HKPD_u8GetPressedKey(){
 
-	u8 Pressed_Key = 20;
+	u8 L_u8PressedKey;
+			for(u8 col=0; col<ColNumber; col++)
+			{
+				MDIO_voidSetPinValue(G_u8PortNum,col,LOW);
+				for(u8 row=0; row<RowNumber; row++)
+				{
+					if(MDIO_u8GetPinValue(G_u8PortNum,row+ColNumber)==LOW)
+					{
+						L_u8PressedKey=G_u8Grid[row][col];
+					}
+					while(MDIO_u8GetPinValue(G_u8PortNum,row+ColNumber)==LOW) {}
+					_delay_ms(10);
 
-	for (u8 col=0; col<4; col++){
-		/*activate current column*/
-
-		MDIO_voidSetPinValue(KPD_PORT, col, LOW);
-
-		for(u8 row=0; row<4; row++){
-			if(MDIO_U8GetPinValue(KPD_PORT,row+4) == 0){
-				Pressed_Key= row+ (col*4) + 1 ;
+				}
+				MDIO_voidSetPinValue(G_u8PortNum,col,HIGH);
 			}
-			while(MDIO_U8GetPinValue(KPD_PORT, row+4) == 0){}
-			_delay_ms(10);
-		}
-		MDIO_voidSetPinValue(KPD_PORT, col, HIGH);
-	}
+			return L_u8PressedKey;	}
 
-	return GKPD_KEYS[Pressed_Key];	}
+
 
